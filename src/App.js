af62e2d0ad7ab723146f805e67459fa1a8d23c85
Cody Tamez <codytamez17@gmail.com>
2020-10-14 20:01:37 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+//import Fetch from './Components/Fetch';
+import ReactDOM from 'react-dom'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
+  let state = {
+    isClicked : false
+  }
+
+  let buttonElement = <button onClick={state.isClicked != state.isClicked}>
+    "Click me!"
+  </button>
+
+  ReactDom.render(
+    buttonElement
   );
 }
+
+
 
 export default App;
